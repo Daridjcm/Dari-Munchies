3eed8fe8f40287fbaafd494c3e263dd077618a0e
Daridjcm <daricorreamedina@gmail.com132844413+Daridjcm@users.noreply.github.com>
2023-12-31 00:17:38 -0500

    Change single quotes to double quotes as the default style of writing a string

--- categories.js
+++ categories.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const categoryLinks = document.querySelectorAll('.selectCategory');
-    const categories = document.querySelectorAll('.category');
+document.addEventListener("DOMContentLoaded", function () {
+    const categoryLinks = document.querySelectorAll(".selectCategory");
+    const categories = document.querySelectorAll(".category");
 
     categoryLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
+        link.addEventListener("click", function (event) {
             event.preventDefault();
 
             // Hide all categories
             categories.forEach(category => {
-                category.style.display = 'none';
+                category.style.display = "none";
             });
 
             // Get the selected category
-            const selectedCategory = this.getAttribute('href');
+            const selectedCategory = this.getAttribute("href");
             const categoryToShow = document.querySelector(selectedCategory);
 
             // Show the selected category
-            categoryToShow.style.display = 'block';
+            categoryToShow.style.display = "block";
 
             // If the selected category is not "allCategories", hide it
-            if (selectedCategory !== '#allCategories') {
-                const todasLasCategorias = document.getElementById('allCategories');
-                todasLasCategorias.style.display = 'none';
+            if (selectedCategory !== "#allCategories") {
+                const todasLasCategorias = document.getElementById("allCategories");
+                todasLasCategorias.style.display = "none";
             }
         });
     });
--- searchProduct.js
+++ searchProduct.js
@@ -1,48 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form[role="search"]');
-    const allCardsContainer = document.querySelector('.row-cols-1.row-cols-md-4.g-4');
-    const allCards = document.querySelectorAll('.card');
-    const suggestionsContainer = document.getElementById('suggestions-container');
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form[role='search']");
+    const allCardsContainer = document.querySelector(".row-cols-1.row-cols-md-4.g-4");
+    const allCards = document.querySelectorAll(".card");
+    const suggestionsContainer = document.getElementById("suggestions-container");
 
-    form.addEventListener('input', function () {
-        const searchTerm = form.querySelector('input[type="search"]').value.toLowerCase();
+    form.addEventListener("input", function () {
+        const searchTerm = form.querySelector("input[type='search']").value.toLowerCase();
 
         const matchingCards = Array.from(allCards).filter(function (card) {
-            const productName = card.querySelector('.card-title').textContent.toLowerCase();
+            const productName = card.querySelector(".card-title").textContent.toLowerCase();
             return productName.includes(searchTerm);
         });
         displaySuggestions(matchingCards);
     });
 
-    form.addEventListener('submit', function (event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault(); 
-        const searchTerm = form.querySelector('input[type="search"]').value.toLowerCase();
+        const searchTerm = form.querySelector("input[type='search']").value.toLowerCase();
 
         allCards.forEach(function (card) {
-            card.style.display = 'none';
+            card.style.display = "none";
         });
 
         const matchingCards = Array.from(allCards).filter(function (card) {
-            const productName = card.querySelector('.card-title').textContent.toLowerCase();
+            const productName = card.querySelector(".card-title").textContent.toLowerCase();
             return productName.includes(searchTerm);
         });
 
         matchingCards.forEach(function (card) {
-            card.style.display = 'block';
+            card.style.display = "block";
             allCardsContainer.appendChild(card);
         });
-        suggestionsContainer.innerHTML = '';
+        suggestionsContainer.innerHTML = "";
     });
 
     function displaySuggestions(suggestedCards) {
-        suggestionsContainer.innerHTML = '';
+        suggestionsContainer.innerHTML = "";
 
         suggestedCards.forEach(function (card) {
-            const suggestion = document.createElement('div');
-            suggestion.className = 'suggestion';
-            suggestion.textContent = card.querySelector('.card-title').textContent;
-            suggestion.addEventListener('click', function () {
-                form.querySelector('input[type="search"]').value = suggestion.textContent;
+            const suggestion = document.createElement("div");
+            suggestion.className = "suggestion";
+            suggestion.textContent = card.querySelector(".card-title").textContent;
+            suggestion.addEventListener("click", function () {
+                form.querySelector("input[type='search']").value = suggestion.textContent;
                 form.submit();
             });
             suggestionsContainer.appendChild(suggestion);
